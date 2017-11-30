a6a3cbcc8e93ce6dbb048ca38027568b6d05a0e3
will <yh@fir.im>
2017-11-30 16:31:50 +0800

    add .

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-BRANCH=feature/$1
+BRANCH=feature/$2
 echo $BRANCH
 git checkout  -b $BRANCH
-echo 1 >> README.rdoc && git add . && git commit  -m "add ." && git push origin  $BRANCH
+echo 1 >> README.rdoc && git add . && git commit  -m "add ." && git push $1  $BRANCH
